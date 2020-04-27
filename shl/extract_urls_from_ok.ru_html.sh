08440f948a4a3014f90293b46861f00cb34a8a31
sebma <sebma@users.noreply.github.com>
2020-04-27 12:46:49 +0200

    Updated extract_urls_from_ok.ru_html.sh

---
+++ shl/extract_urls_from_ok.ru_html.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+scriptBaseName=${0##*/}
+test $# != 1 && {
+	echo "=> Usage $scriptBaseName ok_ru_htmlFile" >&2
+	exit 1
+}
+
+ok_ru_htmlFile=$1
+cat $ok_ru_htmlFile | pup 'json{}' | jq -r 'recurse | arrays[] | select(.class == "video-card_n ellip").title,select(.class == "video-card_lk").href' | awk '{videoRelativeURL=$0;url="https://ok.ru"gensub("?.*$",1,videoRelativeURL); getline title; print url" # "title}'
