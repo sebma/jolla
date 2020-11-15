95d06f8263d0e060df30158176685e911fd507ab
sebma <sebma@users.noreply.github.com>
2020-11-15 19:01:13 +0100

    Updated addMyOpenRepos.sh

--- shl/addMyOpenRepos.sh
+++ shl/addMyOpenRepos.sh
@@ -2,21 +2,18 @@
 
 myOpenRepos="Acce BloodyFoxy CepiPerez Dax DrYak HtheB Larswad MSameer Mariusmssj MartinK Matoking Mister_Magister Morpog NielDK Osanwe Penguin Rikudou_Sennin RobertMe Schturman Sfiet_Konstantin Simom Thaodan V10lator ade backpackjoe balta basil beidl birdzhang bulkin bwalter cnlpete coderus cornedor dcaliste eatdirt edgley ellefj equeim fface forgrimm fravaccaro halftux iKozzz ilpianista inte jabbounet jobe kelvan kimmoli krobelus kuba77 leppa llelectronics lourens marco73f matolainen mikel83 mitakuuluu neochapay omnight osetr r0kk3rz ruedigergad rzr sailfish_lib steffen_f taixzo titilambert tunp vasvlad vevgeniev walokra yoktobit yuvraaj"
 
-set -x
 if which zypper >/dev/null 2>&1;then
 	for repo in $myOpenRepos
 	do
 		repoURL=http://sailfish.openrepos.net/$repo/personal-main.repo
-		sudo zypper addrepo -f $repo
-		sudo zypper refresh -r openrepos-$repo
+		zypper repos | grep -q $repo || { sudo zypper addrepo -f $repo;sudo zypper refresh -r openrepos-$repo; }
 	done
 	zypper repos
 else
 	for repo in $myOpenRepos
 	do
 		repoURL=http://sailfish.openrepos.net/$repo/personal/main
-		ssu repos | grep -q $repoURL || ssu addrepo $repo $repoURL
+		ssu repos | grep -q $repoURL || { ssu addrepo $repo $repoURL; ssu updaterepos $repo; }
 	done
-	ssu updaterepos
 	ssu repos
 fi
