ed9c7d2aef6fd77db34445bbe1d793dd94ebe6b4
sebma <sebma@users.noreply.github.com>
2022-10-04 12:04:11 +0200

    Update jollaUSB_Reconnect.sh

--- shl/jollaUSB_Reconnect.sh
+++ shl/jollaUSB_Reconnect.sh
@@ -2,8 +2,15 @@
 
 set -o nounset
 set -o errexit
+
+if   echo $distribID | egrep "centos|rhel|fedora" -q;then
+	ipcalc=""
+elif echo $distribID | egrep "debian|ubuntu" -q;then
+	ipcalc=ipcalc
+fi
+
 jollaUSBNetwork=192.168.2.14/31
-jollaUSBIP=$(ipcalc $jollaUSBNetwork | awk '/HostMax:/{print$2}')
+jollaUSBIP=$($ipcalc $jollaUSBNetwork | awk '/HostMax:/{print$2}')
 jollaUSBInterface=usb0
 ifconfig $jollaUSBInterface >/dev/null || exit
 if ! route -n | grep -q $jollaUSBInterface
