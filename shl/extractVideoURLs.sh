a7cc503e099a4ad3ff3beb3b885bd00b89da7a12
sebma <sebma@users.noreply.github.com>
2025-02-14 18:57:54 +0100

    Updated extractVideoURLs.sh

--- shl/extractVideoURLs.sh
+++ shl/extractVideoURLs.sh
@@ -1,19 +1,43 @@
 #!/usr/bin/env bash
 
-function extractVideoURLs {
-	for url
+function extractVideoURLsFromWebPage {
+	[ $# != 0 ] && [ "$1" = "-h" ] && {
+		echo "=> Usage $FUNCNAME [-t] url1 url2 url3 ..." >&2
+		return 1
+	}
+
+	time for url
 	do
 		urlBase=$(echo "$url" | cut -d/ -f1-3)
 		fqdn=$(echo "$url" | cut -d/ -f3)
 		domain=$(echo $fqdn | awk -F. '{print$(NF-1)"."$NF}')
 		sld=$(echo $fqdn | awk -F. '{print $(NF-1)}') # Second level domain
-		if [ $sld = dailymotion ];then
+		echo "=> Extracting urls from <$url>..." >&2
+
+		time if [[ "$url" =~ ^[./] ]] || [[ "$url" =~ ^[^/]+$ ]];then # If it is a local file
+			localFile="$url"
+			domain=$(cat "$localFile" | pup 'meta[property=og:url] attr{content}' | awk -F/ '{print$3}')
+			urlPrefix=https://$domain
+			case $domain in
+				ok.ru)
+					time extractedURLs="$(grep -oP "/video/\d+" "$localFile" | uniq | sed "s|^|$urlPrefix|")"
+					i=$(echo "$extractedURLs" | wc -l)
+					echo "=> Extracted $i urls from <$url>." >&2
+					echo "=> Resolving titles from urls..." >&2
+					echo "$extractedURLs"
+				;;
+				*) ;;
+			esac
+		elif [ $sld = dailymotion ];then
 			urlPrefix=${urlBase}/video
 			apiUrl="$url"
-			echo "$url" | \grep -q /playlist || apiUrl="$(echo "$url" | sed "s|$fqdn|$fqdn/user|" )"
+			if echo "$url" | \grep /playlist -q;then
+				apiUrl="${url}/videos?limit=100"
+			else
+				apiUrl="$(echo "$url" | sed "s|$fqdn|$fqdn/user|" )"
+			fi
 			apiUrl="${apiUrl/www/api}"
-			echo "$url" | \grep -q /videos$ || apiUrl="$apiUrl/videos"
-			echo "=> apiUrl = $apiUrl"
+			echo "=> apiUrl = $apiUrl" 1>&2
 			\curl -qs "$apiUrl" | jq -r '"'$urlPrefix/'"+.list[].id'
 		elif [ $sld = vimeo ];then
 			urlPrefix=$urlBase
@@ -21,11 +45,18 @@ function extractVideoURLs {
 			\curl -qs $urlBase/api/v2/$channel/videos.json | jq -r '.[] | "'$urlPrefix/'"+(.id|tostring)'
 		elif [ $sld = youtube ];then
 			urlPrefix=$urlBase
-			\curl -qs "$url" | grep -w video | grep -oP 'href="\K/[^/][^ &"]+' | uniq | sed "s|^|$urlPrefix|"
+			\curl -qs "$url" | grep -oP '"url":"\K/watch\?v=[\d\w-_]{11}' | sort -u | sed "s|^|$urlPrefix|"
+		elif [ $sld = preachub ];then
+			\curl -Lqs "$url" | pup ".pm-pl-list-thumb a attr{href}"
+		elif [ $domain = ok.ru ];then
+			tmpFile=$(mktemp)
+			\wget -qkO $tmpFile "$url"
+			cat $tmpFile | grep -oP "\Khttps?://ok.ru/video/\d+" | uniq
+			rm -f $tmpFile
 		else
 			:
 		fi
 	done
 }
 
-extractVideoURLs "$@"
+extractVideoURLsFromWebPage "$@"
